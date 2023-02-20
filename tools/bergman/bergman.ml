c42a3abfde6566d1b890b48b4f764581be733d9e
Samuel Mimram <smimram@gmail.com>
2023-02-20 10:51:38 +0100

    Gröbner

--- tools/bergman/bergman.ml
+++ tools/bergman/bergman.ml
@@ -1,3 +1,45 @@
+open Alg
+
+module M = Monoid.Free(Alphabet.Char)
+module K = Field.Int
+module P = Algebra.Pres(K)(Alphabet.Char)
+module A = P.A
+
+let char_of_string s =
+  assert (String.length s = 1);
+  s.[0]
+
+let parse_pol s =
+  Parser.main Lexer.token (Lexing.from_string s)
+
+let eval_pol p =
+  let rec aux = function
+    | Pol.Mul (p,q) -> A.mul (aux p) (aux q)
+    | One -> A.one
+    | Zero -> A.zero
+    | Gen c -> A.inj (M.inj c)
+    | Add (p,q) -> A.add (aux p) (aux q)
+    | Sub (p,q) -> A.sub (aux p) (aux q)
+    | Neg p -> A.neg (aux p)
+  in
+  aux p
+
 let () =
+  let order = ref "deglex" in
+  let variables = ref "" in
   let relations = ref "" in
-  Arg.parse [] (fun s -> relations := s) "Usage: bergman [options]"
+  Arg.parse [
+    "--variables", Arg.Set_string variables, "Used variables."
+  ] (fun s -> relations := s) "Usage: bergman [options]";
+  let variables = !variables |> String.split_on_char ',' |> List.map (fun s -> s |> String.trim |> char_of_string) in
+  let relations = !relations |> String.split_on_char ',' |> List.map (fun s -> s |> String.trim |> parse_pol |> eval_pol) in
+  let order =
+    match !order with
+    | "deglex" -> M.Order.deglex Alphabet.Char.leq
+    | "revdeglex" -> M.Order.deglex Alphabet.Char.geq
+    | _ -> assert false
+  in
+  let pres = P.make order variables relations in
+  Printf.printf "# Computing Gröbner basis\n\n%!";
+  let pres = P.reduce (P.buchberger pres) in
+  ignore pres
