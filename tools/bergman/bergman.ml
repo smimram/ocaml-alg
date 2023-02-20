a354365f7efc6611012e578ec22760347f36bc7f
Samuel Mimram <smimram@gmail.com>
2023-02-20 10:17:42 +0100

    Remove Genlex code.

--- tools/bergman/bergman.ml
+++ tools/bergman/bergman.ml
@@ -21,63 +21,6 @@ exception Parsing
 let parse_pol s =
   Parser.main Lexer.token (Lexing.from_string s)
 
-  (*
-  let lexer = Genlex.make_lexer ["+";"-";"*";"^";"(";")"] in
-  let rec mults = function
-    | [] -> `One
-    | [x] -> x
-    | x::l -> `Mul (x, mults l)
-  in
-  fun s ->
-    let int n =
-      if n = 0 then `Zero
-      else if n = 1 then `One
-      else if n = -1 then `Neg `One
-      else raise Parsing
-    in
-    let rec add = parser
-      | [< p = mul ; q = adds >] -> q p
-    and adds = parser
-        | [< 'Genlex.Kwd "+"; q = mul; r = adds >] -> fun p -> r (`Add (p,q))
-        | [< 'Genlex.Kwd "-"; q = mul; r = adds >] -> fun p -> r (`Sub (p,q))
-        | [< 'Genlex.Int n ; r = adds >] -> fun p ->
-          if n >= 0 then raise Parsing
-          else r (`Sub (p,int (-n)))
-        | [< >] -> fun p -> p
-    and mul = parser
-        | [< p = exp ; q = muls >] -> q p
-        | [< p = exp >] -> p
-    and muls = parser
-        | [< 'Genlex.Kwd "*"; q = exp; r = muls >] -> fun p -> r (`Mul (p,q))
-        | [< >] -> fun p -> p
-    and exp = parser
-        | [< p = atom; q = exps >] -> q p
-    and exps = parser
-        | [< 'Genlex.Kwd "^" ; 'Genlex.Int n >] ->
-           fun p ->
-             let rec aux = function
-               | 0 -> `One
-               | 1 -> p
-               | n -> `Mul (p, aux (n-1))
-             in
-             aux n
-        | [< >] -> fun p -> p
-    and atom = parser
-        | [< 'Genlex.Char c >] ->
-           `Gen c
-        | [< 'Genlex.Ident s >] ->
-           let ans = ref [] in
-           for i = String.length s - 1 downto 0 do
-             ans := (`Gen s.[i]) :: !ans
-           done;
-           mults !ans
-        | [< 'Genlex.Int n >] -> int n
-        | [< 'Genlex.Kwd "-"; p = atom >] -> `Neg p
-        | [< 'Genlex.Kwd "("; p = add; 'Genlex.Kwd ")" >] -> p
-    in
-    add (lexer (Stream.of_string s))
-   *)
-
 let char_of_string s =
   assert (String.length s = 1);
   s.[0]
