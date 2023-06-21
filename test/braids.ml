bd5ba6795f0b539c926519d4e1e85c0c9c5db809
Samuel Mimram <smimram@gmail.com>
2023-06-21 16:24:00 +0200

    Braids example.

---
+++ test/braids.ml
@@ -0,0 +1,23 @@
+open Alg
+
+module X = struct
+  include Alphabet.Int
+  let to_string n = String.make 1 (char_of_int (int_of_char 'a' + n))
+end
+module P = Monoid.Pres(X)
+
+let () =
+  let a = 0 in
+  let b = 1 in
+  let c = 2 in
+  let pres =
+    P.make [a;b;c] [
+    [|a;b;a|],[|b;a;b|];
+    [|a;b|],[|c|];
+  ]
+  in
+  print_endline ("presentation: " ^ P.to_string pres);
+  let pres = P.complete (P.W.Order.deglex X.leq) pres in
+  print_endline ("completed: " ^ P.to_string pres);
+  let pres = P.reduce pres in
+  print_endline ("reduced: " ^ P.to_string pres);
