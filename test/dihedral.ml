91b7dd4db6eb96424e294a7da9a6105551d131f8
Samuel Mimram <smimram@gmail.com>
2024-02-05 20:30:23 +0100

    Dihedral.

--- test/dihedral.ml
+++ test/dihedral.ml
@@ -10,16 +10,25 @@ module W = P.W
 let () =
   let r = 0 in
   let s = 1 in
-  let pres n =
+  let presentation n =
     P.make [r;s] [
       W.pow (W.inj r) n, W.one;
       W.pow (W.inj s) 2, W.one;
       [|r;s|], W.mul (W.inj s) (W.pow (W.inj r) (n-1))
     ]
   in
-  let pres = pres 6 in
+  let pres = presentation 4 in
   print_endline ("presentation: " ^ P.to_string pres);
   let pres = P.complete (P.W.Order.deglex X.leq) pres in
   print_endline ("completed: " ^ P.to_string pres);
   let pres = P.reduce pres in
-  print_endline ("reduced: " ^ P.to_string pres)
+  print_endline ("reduced: " ^ P.to_string pres);
+  print_newline ();
+  for i = 1 to 10 do
+    presentation i
+    |> P.complete (P.W.Order.deglex X.leq)
+    |> P.reduce
+    |> P.to_string
+    |> Printf.printf "D%02d: %s\n%!" i
+  done
+      
